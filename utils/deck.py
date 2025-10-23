e4773aa85d7c3cf839cee398f8c9e8a176e4510e
דוד פורסט <dforst@g.jct.ac.il>
2025-10-23 09:54:30 +0300

    Create all files and function

---
+++ utils/deck.py
@@ -0,0 +1,10 @@
+def create_card(rank:str,suite:str) -> dict
+
+
+def compare_cards(p1_card:dict, p2_card:dict) -> str
+
+
+def create_deck() -> list[dict]
+
+
+def shuffle(deck:list[dict]) -> list[dict]
